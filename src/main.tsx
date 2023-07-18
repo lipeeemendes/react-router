61a08c1f12a738e25d39eb9f9c591f541ca58e2c
lipeeemendes <felipemendesofficail@gmail.com>
2023-07-18 11:51:35 -0300

    Estilização

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import App from "./App.tsx";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page.tsx";
+import Contact from "./routes/contact.tsx";
+import Root, { loader as rootLoader } from "./routes/root.tsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
