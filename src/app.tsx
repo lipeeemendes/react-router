58eb7d8270fe6354809fd6a198ae018e0c22d19d
lipeeemendes <felipemendesofficail@gmail.com>
2023-07-21 10:47:20 -0300

    Consumindo api rest com useEffect

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { TODOS } from "./services/api";
 import type { Todo } from "./services/api";
 
+// Função principal App
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>();
+  // Desestruturação de uma array para o gerencimento do estado da lista
   useEffect(() => {
     const getTodos = async () => {
       const todo = await TODOS();
@@ -14,8 +16,11 @@ export default function App() {
   return (
     <>
       <h1>App</h1>
+      {/* Pegando a lista de todos em JSON e transformando em string, o segudo
+       prarmetro pode ser uma função e o terceiro indentação */}
       <pre>{JSON.stringify(todos, null, 2)}</pre>
       <ul>
+        {/* Passando por cada item do todo e renderizando o titulo do todo */}
         {todos?.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // Renderização dos componentes
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); // <React.StrictMode> É o componente com correções rigorosas
